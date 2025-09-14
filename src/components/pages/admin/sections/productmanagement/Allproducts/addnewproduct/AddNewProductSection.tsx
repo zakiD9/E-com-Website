eb70a6b403620e8086fe0e69e6fb1475fc6ad50b
zakiD9 <zakariadjerboa@gmail.com>
2025-09-14 22:24:00 +0100

    preparing add new product section

---
+++ src/components/pages/admin/sections/productmanagement/Allproducts/addnewproduct/AddNewProductSection.tsx
@@ -0,0 +1,17 @@
+
+
+
+
+export default function AddNewProductSection(){
+
+    return(
+        <div className="flex flex-col p-4 gap-10">
+            <div className="flex gap-2">
+                
+            </div>
+            <div className="flex">
+
+            </div>
+        </div>
+    )
+}
